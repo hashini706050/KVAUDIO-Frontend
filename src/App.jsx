0eb1425a5b58587f8e938dbe4b1f9bbeecfd1be0
HashiniJ <it22171924@my.sliit.lk>
2025-06-22 22:40:26 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
 import ProductCard from './components/productCard'
 
+
 function App() {
   
 
   return (
-    <div>
-      <ProductCard name="JBL Audio" price="3250/-" description="gf yyy tt gg vv"
-      photoUrl="https://img.drz.lazcdn.com/g/kf/Se215720a4c364a6f81388628dc7a99d5k.jpg_400x400q75.avif"/>
+    <div className='w-full h-screen flex'>
+      <div className='w-[300px] h-full bg-green-200'>
+        <button className='w-full h-[40px] text-[25px] font-bold'>
+          Dashboard
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold'>
+          Items
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold'>
+          Reviews
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold'>
+         Users
+        </button>
+      </div>
+      <div className='w-full bg-red-900'>
+        
 
+      </div>
+      
     </div>
       
   
